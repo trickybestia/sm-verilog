728d9a5a12f6daa50060b28604be66a621126159
trickybestia <trickybestia@gmail.com>
2023-08-19 13:31:23 +0300

    Add create_disk - a program to create blueprint with "disk" with file content

---
+++ create_disk.py
@@ -0,0 +1,60 @@
+import argparse
+from pathlib import Path
+
+from create_blueprint.blueprint import Blueprint
+from create_blueprint.shapes import ShapeId
+
+ZERO_BIT_COLOR = "222222"
+ONE_BIT_COLOR = "EEEEEE"
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        "create_disk",
+        description="Creates blueprint with rectangle with painted bits from specified file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("file", type=Path)
+    parser.add_argument("-w", "--stripe-width", type=int)
+    parser.add_argument(
+        "-b",
+        "--blueprints-path",
+        help="path to a directory in which blueprints will be stored",
+        type=Path,
+        default="./blueprints/",
+    )
+
+    args = parser.parse_args()
+
+    (args.blueprints_path / args.file.name).mkdir(parents=True, exist_ok=True)
+
+    blueprint = Blueprint()
+
+    blueprint.name = args.file.name
+
+    blueprint.create_solid(ShapeId.Concrete, -1, 0, 0, color="D02525")
+
+    bit_offset = 0
+
+    for byte in args.file.read_bytes():
+        for i in range(8):
+            bit = byte & 1
+            byte >>= 1
+            block_color = ONE_BIT_COLOR if bit else ZERO_BIT_COLOR
+
+            x = bit_offset
+            z = 0
+
+            if args.stripe_width is not None:
+                x = bit_offset % args.stripe_width
+                z = bit_offset // args.stripe_width
+
+            blueprint.create_solid(ShapeId.Concrete, x, 0, z, color=block_color)
+
+            bit_offset += 1
+
+    blueprint.save(args.blueprints_path / args.file.name)
+
+
+main()
