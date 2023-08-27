baafea02dc90254dea0f5c8ff72bc7a8abf5ad2a
trickybestia <trickybestia@gmail.com>
2023-08-27 19:53:49 +0300

    Split IdGenerator "next" method to "next" and "next_range"

--- create_blueprint/id_generator.py
+++ create_blueprint/id_generator.py
@@ -1,18 +1,19 @@
-from typing import Union
-
-
 class IdGenerator:
     _id: int
 
     def __init__(self) -> None:
         self._id = 0
 
-    def next(self, size: Union[int, None] = None) -> Union[int, range]:
-        if size is None:
-            result = self._id
-            self._id += 1
-        else:
-            result = range(self._id, self._id + size)
-            self._id += size
+    def next_range(self, size: int) -> range:
+        result = range(self._id, self._id + size)
+
+        self._id += size
+
+        return result
+
+    def next(self) -> int:
+        result = self._id
+
+        self._id += 1
 
         return result
