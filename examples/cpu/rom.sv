8d958b0e95588507521278b72627ab0149115bec
trickybestia <trickybestia@gmail.com>
2023-09-01 21:58:07 +0300

    Infer MEM_SIZE from ADDR_WIDTH in rom

--- examples/cpu/rom.sv
+++ examples/cpu/rom.sv
@@ -1,8 +1,8 @@
-module rom #(parameter ADDR_WIDTH = 8, parameter MEM_SIZE = 32) (
+module rom #(parameter ADDR_WIDTH = 8) (
     input  bit  [ADDR_WIDTH-1:0] address,
 
     (* attachment="sensor", stripe_width=8, override_x=1, override_y=1, override_z=3 *)
-    input  bit  [MEM_SIZE*8-1:0] mem,
+    input  bit  [(2**ADDR_WIDTH)*8-1:0] mem,
 
     output bit [15:0]     read_value
 );
