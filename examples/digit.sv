359167e668805616237c55e525659fb0e0c11b03
trickybestia <trickybestia@gmail.com>
2023-09-01 21:37:43 +0300

    Clean up digit_display.sv Move digit_t and empty_digit definitions to digit.sv

---
+++ examples/digit.sv
@@ -0,0 +1,3 @@
+typedef bit [3:0] digit_t;
+
+localparam empty_digit = 10;
--- examples/digit_display.sv
+++ examples/digit_display.sv
@@ -1,7 +1,27 @@
-typedef bit [3:0] digit_t;
+`include "digit.sv"
 
-parameter empty_digit = 10;
+localparam bit [11*7-1:0] DIGITS_PATTERNS = {
+    7'b0000000, // empty_digit
+    7'b1111101, // 9
+    7'b1111111, // 8
+    7'b0111000, // 7
+    7'b1101111, // 6
+    7'b1101101, // 5
+    7'b1011001, // 4
+    7'b0111101, // 3
+    7'b0110111, // 2
+    7'b0011000, // 1
+    7'b1111110  // 0
+};
 
+function bit [6:0] digit_pattern(digit_t digit);
+    digit_t effective_digit;
+
+    if (digit > empty_digit) effective_digit = empty_digit;
+    else effective_digit = digit;
+    
+    digit_pattern = DIGITS_PATTERNS[7*effective_digit+:7];
+endfunction
 
 module digit_display(
     input  digit_t digit,
@@ -9,22 +29,6 @@ module digit_display(
     output bit top_left, top, top_right, bottom_right, bottom, bottom_left, middle
 );
     always begin
-        top_left = 0;
-        top = 0;
-        top_right = 0;
-        bottom_right = 0;
-        bottom = 0;
-        bottom_left = 0;
-        middle = 0;
-
-        if (digit != empty_digit) begin
-            top_left = digit == 0 || digit == 4 || digit == 5 || digit == 6 || digit == 8 || digit == 9;
-            top = digit == 0 || digit == 2 || digit == 3 || digit == 5 || digit == 6 || digit == 7 || digit == 8 || digit == 9;
-            top_right = digit == 0 || digit == 1 || digit == 2 || digit == 3 || digit == 4 || digit == 7 || digit == 8 || digit == 9;
-            bottom_right = digit == 0 || digit == 1 || digit == 3 || digit == 4 || digit == 5 || digit == 6 || digit == 7 || digit == 8 || digit == 9;
-            bottom = digit == 0 || digit == 2 || digit == 3 || digit == 5 || digit == 6 || digit == 8 || digit == 9;
-            bottom_left = digit == 0 || digit == 2 || digit == 6 || digit == 8;
-            middle = digit == 2 || digit == 3 || digit == 4 || digit == 5 || digit == 6 || digit == 8 || digit == 9;
-        end
+        {top_left, top, top_right, bottom_right, bottom, bottom_left, middle} = digit_pattern(digit);
     end
 endmodule
